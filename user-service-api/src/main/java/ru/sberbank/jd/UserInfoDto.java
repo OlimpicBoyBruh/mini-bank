b4b69b87a8b594a7b5a08fb2f8d5c6dc8566544d
Danila.Tatara <dtatara@gmail.com>
2024-02-15 01:58:30 +0300

    Pulled out user-service DTO for external use

---
+++ user-service-api/src/main/java/ru/sberbank/jd/UserInfoDto.java
@@ -0,0 +1,25 @@
+package ru.sberbank.jd;
+
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.Data;
+
+/**
+ * User info exchange object.
+ */
+@Data
+public class UserInfoDto {
+
+    private UUID id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String phone;
+    @NotBlank
+    private LocalDate birthDate;
+}
