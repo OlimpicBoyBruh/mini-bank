dd8457fde66b2c6969b41cf0cf0e8dbe68209b9d
Danila.Tatara <dtatara@gmail.com>
2024-02-24 14:46:12 +0300

    Corrections to docker files and project props

--- account-service/src/main/java/ru/sberbank/jd/App.java
+++ account-service/src/main/java/ru/sberbank/jd/App.java
@@ -2,11 +2,13 @@ package ru.sberbank.jd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Старт приложения.
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.sberbank.jd"})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
