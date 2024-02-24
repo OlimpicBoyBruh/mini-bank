c38bc5d1ae50af88738fc3b78364c5500708c2b1
Danila.Tatara <dtatara@gmail.com>
2024-02-24 15:38:36 +0300

    Changed settings of operation service to connect in docker

--- operation-service/src/main/java/ru/sberbank/jd/properties/AccountServiceIntegrationProperties.java
+++ operation-service/src/main/java/ru/sberbank/jd/properties/AccountServiceIntegrationProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 //@ConstructorBinding
-@ConfigurationProperties(prefix = "integration.account-service")
+@ConfigurationProperties(prefix = "integration.account.service")
 @ConfigurationPropertiesScan
 @Data
 public class AccountServiceIntegrationProperties {
