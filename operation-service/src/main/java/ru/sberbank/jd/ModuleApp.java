8d542f260dcddff5bd5a71edd32b43beb2d9a554
Danila.Tatara <dtatara@gmail.com>
2024-02-24 15:04:28 +0300

    Containerized operation service, some corrections to pom's

--- operation-service/src/main/java/ru/sberbank/jd/ModuleApp.java
+++ operation-service/src/main/java/ru/sberbank/jd/ModuleApp.java
@@ -2,8 +2,10 @@ package ru.sberbank.jd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.sberbank.jd"})
 public class ModuleApp {
 
 	public static void main(String[] args) {
